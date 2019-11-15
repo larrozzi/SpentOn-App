762c4af55defd35875099bd2607c488e750ba15f
Yassine <larrozzi@gmail.com>
2019-11-14 20:54:25 -0800

    webpack production

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,47 @@
-
 const path = require('path');
 
-module.exports = {
-  entry: './src/app.js',
-  // entry: './src/playground/redux101.js',
-  // entry:'./src/playground/redux-spenton.js',
-  //entry: './src/playground/descructuring.js',
-  //entry: './src/playground/hoc.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  }, 
-module:{
-    rules:[{  
-        loader: 'babel-loader', //making webpack run babel for us
-        test: /\.js$/ , //regular expression to specify which files to use babel on
-        exclude: /node_modules/
-    },{
-      use :[ // for loading an array of loader
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ],
-      test:/\.s?css$/ 
-    }]
-},
+module.exports = (env,argv) => {
+  //console.log('env',env)
+  const isProduction = argv.mode === 'production'
+    //const isProduction = env === 'production'
+  return {
+    entry: './src/app.js',
+    // entry: './src/playground/redux101.js',
+    // entry:'./src/playground/redux-spenton.js',
+    //entry: './src/playground/descructuring.js',
+    //entry: './src/playground/hoc.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    }, 
+    module:{
+        rules:[{  
+            loader: 'babel-loader', //making webpack run babel for us
+            test: /\.js$/ , //regular expression to specify which files to use babel on
+            exclude: /node_modules/
+        },{
+          use :[ // for loading an array of loader
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+          ],
+          test:/\.s?css$/ 
+        }]
+    },
 
-devtool:'cheap-module-eval-source-map',
-devServer:{
-    contentBase:path.join(__dirname, 'public'),
-    historyApiFallback:true
+  devtool:isProduction ?'source-map' :'cheap-module-eval-source-map',
+  // if (argv.mode === 'development') {
+  //   devtool:'cheap-module-eval-source-map';
+  // }
+  // if (argv.mode === 'production'){
+  // devtool:'source-map';
+  // }
+    
+    devServer:{
+        contentBase:path.join(__dirname, 'public'),
+        historyApiFallback:true
+    }
+  }
 }
-};
 
 //everytime you reconfigure this you need to execute "yarn run build"
