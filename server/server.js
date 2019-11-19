19289de51c8bae004d1c190cffc19e0bb5cd8801
Yassine <larrozzi@gmail.com>
2019-11-18 22:09:37 -0800

    cleared yarn cache

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const path = require('path') //node way of importing
 const express = require('express')
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public') //dirname is server, go up an folder, go to public
-const port = process.env.PORT || 3000 ;// PORT is the environment var that heroku sets for you
+const port = (process.env.YOUR_PORT || process.env.PORT || 3000) ;// PORT is the environment var that heroku sets for you
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
 app.use(express.static(publicPath));
 
 //serve path requested that's not in public directory
@@ -11,6 +12,6 @@ app.get('*', (req, res)=>{ //* any path that produces a 404
     res.sendFile(path.join(publicPath,'index.html'))
 }) 
 
-app.listen(port, () => {
-    console.log('server is up')
+app.listen(port,server_host, () => {
+    console.log(`server is up in port: ${port}` )
 });
