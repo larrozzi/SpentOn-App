f696568404fb7c87caef70e9e1a595f81786a25e
Yassine <larrozzi@gmail.com>
2019-11-18 17:43:06 -0800

    new deployement

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path') //node way of importing
 const express = require('express')
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public') //dirname is server, go up an folder, go to public
-const port =process.env.PORT || 3000 ;// PORT is the environment var that heroku sets for you
+const port = process.env.PORT || 3000 ;// PORT is the environment var that heroku sets for you
 app.use(express.static(publicPath));
 
 //serve path requested that's not in public directory
