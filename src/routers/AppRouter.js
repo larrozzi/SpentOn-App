f4de47eb839507c6d521bccd6e1361b0fd623b06
Yassine <larrozzi@gmail.com>
2020-05-05 19:30:14 -0400

    adding PrivateRoute

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import {Router, Route,Switch, Link, NavLink}from 'react-router-dom';
 
-import Header from '../components/Header';
 import SpentonDashboardPage from '../components/SpentonDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
-import HelpPage from '../components/HelpPage';
+//import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 import LoginPage from '../components/LoginPage'; 
 //import {createHistory} from 'history/createBrowserHistory' 
 import {createBrowserHistory} from 'history' 
+import PrivateRoute from './PrivateRoute'
 
 export const history = createBrowserHistory();
 
 const AppRouter =()=>(
     <Router history = {history}> {/* this requires a single root element */} 
         <div>
-        <Header/> 
             <Switch>
                 <Route path="/" component={LoginPage} exact={true}/>
-                <Route path="/dashboard" component ={SpentonDashboardPage} />  
-                <Route path="/create" component={AddExpensePage} />
-                <Route path="/edit/:id" component={EditExpensePage} /*:id is used for variables  */ />
+                <PrivateRoute path="/dashboard" component ={SpentonDashboardPage} />  
+                <PrivateRoute path="/create" component={AddExpensePage} />
+                <PrivateRoute path="/edit/:id" component={EditExpensePage} /*:id is used for variables  */ />
                 {/*<Route path="/help" component={HelpPage} />*/}
                 <Route component ={NotFoundPage}/>
             </Switch>
