67b9f44ab75834a9e83ceed9ec4ca45a2e43d9a5
Yassine <larrozzi@gmail.com>
2020-05-22 02:50:23 -0400

    -

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,7 @@ module.exports = (env,argv) => {
   const isDev = argv.mode !== 'production'
   //const isProduction = env === 'production'
   return {
-    entry: './src/app.js',
-    //entry: ['babel-polyfill','./src/app.js'],
+    entry: ['babel-polyfill','./src/app.js'],
     output: {
       path: path.join(__dirname, 'public','dist'),
       filename: 'bundle.js'
